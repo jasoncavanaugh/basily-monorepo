cd982b9b955a2574c51c3dc541c2f74edf1defdf
Jason Cavanaugh <jascav418@gmail.com>
2025-11-30 16:52:02 -0500

    Reorg stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,26 +15,25 @@ function App() {
 }
 
 function AppInner() {
-  return <div>Here is another piece of text.</div>;
-  // const qry = useQuery({
-  //   queryKey: ["/api/test"],
-  //   queryFn: async () => {
-  //     const resp = await fetch(
-  //       `https://${import.meta.env.VITE_BACKEND_HOST}/api/test`
-  //     );
-  //     if (!resp.ok) {
-  //       throw new Error();
-  //     }
-  //     return resp.json();
-  //   },
-  // });
-  // if (qry.status === "error") {
-  //   return <div>Error...</div>;
-  // }
-  // if (qry.status === "pending") {
-  //   return <div>Loading...</div>;
-  // }
-  // return <div>{JSON.stringify(qry.data)}</div>;
+  const qry = useQuery({
+    queryKey: ["/api/test"],
+    queryFn: async () => {
+      const resp = await fetch(
+        `https://${import.meta.env.VITE_BACKEND_HOST}/api/test`
+      );
+      if (!resp.ok) {
+        throw new Error();
+      }
+      return resp.json();
+    },
+  });
+  if (qry.status === "error") {
+    return <div>Error...</div>;
+  }
+  if (qry.status === "pending") {
+    return <div>Loading...</div>;
+  }
+  return <div>{JSON.stringify(qry.data)}</div>;
 }
 
 export default App;
