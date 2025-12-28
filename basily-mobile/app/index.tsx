21ae3426458478c1f899ab4501180a229e11ddbf
Jason Cavanaugh <jascav418@gmail.com>
2025-12-28 10:26:55 -0500

    Load assets properly in mobile

--- basily-mobile/app/index.tsx
+++ basily-mobile/app/index.tsx
@@ -1,74 +1,65 @@
 import { useAssets } from "expo-asset";
 import { Image } from "expo-image";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function Index() {
-  const [assets, error] = useAssets([require("../assets/images/logo.png")]);
-  console.log("assets", assets, error);
+  const [assets] = useAssets([require("../assets/images/logo.png")]);
 
   return (
     <View className="flex h-[100vh] flex-col pt-8 md:flex-row md:items-center md:justify-center md:py-4">
-      <View className="flex h-[20%] flex-col items-start justify-center gap-3 rounded-lg  px-4 py-8 md:h-[100%] md:w-[50%] md:gap-6 md:p-16">
-        <View className="flex items-center gap-2">
-          <View style={styles.container}>
+      <View className="flex flex-col items-start justify-center gap-3 rounded-lg px-4 py-8 md:h-[100%] md:w-[50%] md:gap-6 md:p-16">
+        <View className="flex flex-row gap-2">
+          <View className="h-12 w-12">
             {assets ? (
               <Image
-                style={styles.image}
-                source="https://picsum.photos/seed/696/3000/2000"
+                style={{ flex: 1, width: "100%" }}
+                source={assets[0].uri}
+                alt="Wth"
                 placeholder={{ blurhash }}
                 contentFit="cover"
                 transition={1000}
               />
-            ) : (
-              <Text>Nope</Text>
-            )}
+            ) : null}
           </View>
-          <Text className="text-6xl font-medium tracking-wide">BASILY</Text>
+          <View className="flex items-center gap-2">
+            <Text className="text-6xl font-medium tracking-wide">BASILY</Text>
+          </View>
         </View>
         <Text className="text-base text-slate-700 dark:text-white md:text-2xl md:text-lg">
           A minimalistic expense tracker
         </Text>
+        <Pressable
+          className="rounded-full bg-squirtle px-3 py-1 text-sm font-semibold text-white shadow-sm shadow-blue-300 hover:brightness-110 w-20 md:w-24 dark:bg-rengar md:px-6 md:py-2 md:text-3xl md:text-lg"
+          onPress={() => {
+            // void auth.signIn.social(
+            //   {
+            //     provider: "github",
+            //     callbackURL: `exp://192.168.1.150:8081/expenses`,
+            //     errorCallbackURL: `exp://192.168.1.150:8081/sign-in`,
+            //   },
+            //   {
+            //     onRequest: () => {
+            //       set_sign_in_loading(true);
+            //     },
+            //     onError: () => {
+            //       alert("Something went wrong");
+            //       set_sign_in_loading(false);
+            //     },
+            //   },
+            // );
+          }}
+        >
+          <Text className="text-white font-semibold">Sign In</Text>
+          {/* {sign_in_loading ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text className="text-white font-semibold">Sign In</Text>
+          )} */}
+        </Pressable>
       </View>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#0553',
-  },
-});
-/*
-    <div className="flex h-[100vh] flex-col pt-8 md:flex-row md:items-center md:justify-center md:py-4">
-      <div className="flex h-[20%] flex-col items-start justify-center gap-3 rounded-lg  px-4 py-8 md:h-[100%] md:w-[50%] md:gap-6 md:p-16">
-        <Logo />
-        <p className="text-base text-slate-700 dark:text-white md:text-2xl md:text-lg">
-          A minimalistic expense tracker
-        </p>
-        <button
-          className="rounded-full bg-squirtle px-3 py-1 text-sm font-semibold text-white shadow-sm shadow-blue-300 hover:brightness-110 dark:bg-rengar md:px-6 md:py-2 md:text-3xl md:text-lg"
-          onClick={() =>
-            void auth_client.signIn.social({
-              provider: "github",
-              callbackURL: `${FRONTEND_URL}/expenses`, 
-              errorCallbackURL: `${FRONTEND_URL}/sign-in`,
-            })
-          }
-        >
-          Sign In
-        </button>
-      </div>
-      <BasilPreview />
-    </div>
-
-*/
