ee4fc1099d0660c8763c2377ebe12fde1a7f9fe6
Jason Cavanaugh <jascav418@gmail.com>
2025-12-28 10:28:15 -0500

    prettier --write **/app/**

--- basily-mobile/app/index.tsx
+++ basily-mobile/app/index.tsx
@@ -3,7 +3,7 @@ import { Image } from "expo-image";
 import { Pressable, Text, View } from "react-native";
 
 const blurhash =
-  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function Index() {
   const [assets] = useAssets([require("../assets/images/logo.png")]);
